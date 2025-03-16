975a5fa2a4658b2af86aadc29e1d6a20e21fceb0
samlichhan <lic287962@gmail.com>
2025-03-16 15:01:42 +0800

    ff

--- js/language.js
+++ js/language.js
@@ -4,6 +4,7 @@ const trans = {
   en: {
     // 首頁
     twelcome: "Hello, my name is <span class='name'>Sam Li</span>",
+    // tican: "I can serve as <span class='typing'></span>",
     tsintro:
       "I am a programmer with over one year of internship experience, specializing in front-end and back-end engineering, creating applications using Python, creating visual charts using PowerBI, and more",
     tab_home: "<i class='fa fa-home'></i> Home",
@@ -52,6 +53,7 @@ const trans = {
   zh: {
     // 首頁
     twelcome: "你好，我是<span class='name'>李承翰</span>",
+    // tican: "我會 <span class='typing'></span>",
     tsintro:
       "我是一名程式設計師，擁有超過一年實習經驗，專注於前端和後端開發，熟悉使用 Python 開發應用程式、使用 PowerBI 創建視覺化圖表等。",
     tab_home: "<i class='fa fa-home'></i> 首頁",
@@ -102,10 +104,10 @@ let currentLanguage = "en";
 
 typed = new Typed(".typing", {
   strings: [
-    currentLanguage === "zh" ? "全端工程師" : "Full-Stack Engineer",
-    currentLanguage === "zh" ? "後端工程師" : "Back-End Engineer",
-    currentLanguage === "zh" ? "前端工程師" : "Front-End Engineer",
-    currentLanguage === "zh" ? "應用程式開發師" : "Application Developer",
+    currentLanguage === "zh" ? "全端工程" : "Full-Stack Engineer",
+    currentLanguage === "zh" ? "後端工程" : "Back-End Engineer",
+    currentLanguage === "zh" ? "前端工程" : "Front-End Engineer",
+    currentLanguage === "zh" ? "應用程式開發" : "Application Developer",
   ],
   typeSpeed: 20,
   backSpeed: 10,
